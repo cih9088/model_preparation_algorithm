bbae530a5f13335e19f0120e10797fdfeaec392b
Songki Choi <songki.choi@intel.com>
2022-10-25 15:21:40 +0900

    detection_tasks -> otx
    Signed-off-by: Songki Choi <songki.choi@intel.com>

--- mpa/det/trainer.py
+++ mpa/det/trainer.py
@@ -19,8 +19,8 @@ from torch import nn
 
 from .stage import DetectionStage
 
-# TODO[JAEGUK]: Remove import detection_tasks
-# from detection_tasks.apis.detection.config_utils import cluster_anchors
+# TODO[JAEGUK]: Remove import otx
+from otx.algorithms.detection.adapters.mmdet.utils import cluster_anchors
 
 from mpa.registry import STAGES
 from mpa.modules.utils.task_adapt import extract_anchor_ratio
@@ -120,13 +120,12 @@ class DetectionTrainer(IncrDetectionStage):
             self._modify_cfg_for_distributed(model, cfg)
 
         # Do clustering for SSD model
-        # TODO[JAEGUK]: Temporary disable cluster_anchors for SSD model
-        # if hasattr(cfg.model, 'bbox_head') and hasattr(cfg.model.bbox_head, 'anchor_generator'):
-        #     if getattr(cfg.model.bbox_head.anchor_generator, 'reclustering_anchors', False):
-        #         train_cfg = Stage.get_data_cfg(cfg, "train")
-        #         train_dataset = train_cfg.get('otx_dataset', None)
-        #         cfg, model = cluster_anchors(cfg, train_dataset, model)
-
+        # TODO[JAEGUK]: Temporal Disable cluster_anchors for SSD model
+        if hasattr(cfg.model, 'bbox_head') and hasattr(cfg.model.bbox_head, 'anchor_generator'):
+            if getattr(cfg.model.bbox_head.anchor_generator, 'reclustering_anchors', False):
+                train_cfg = Stage.get_train_data_cfg(cfg)
+                train_dataset = train_cfg.get('otx_dataset', None)
+                cfg, model = cluster_anchors(cfg, train_dataset, model)
         train_detector(
             model,
             datasets,
